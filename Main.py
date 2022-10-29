8bbaf83ffb56da9e41e4d7d9e73b72d6a6f333c0
Dar'ya <ZarinStar@users.noreply.github.com>
2022-10-29 14:47:33 -0400

    Emoji support and Theotoko's Fast

--- Main.py
+++ Main.py
@@ -3,10 +3,11 @@
 # To run in powershell: py Main.py3
 
 import orthodoxcalendar
+import emoji #https://unicode.org/emoji/charts/emoji-list.html
 
 inputString = input("Welcome, what month and year do you want to view? (MM YYYY): ")
 
 calendarList = orthodoxcalendar.monthList(inputString)
 
 for day in calendarList:
-	print(day)
+	print(emoji.emojize(day))
--- orthodoxcalendar.py
+++ orthodoxcalendar.py
@@ -4,17 +4,41 @@
 # Note: This uses the 'New calendar' instead of the Old calendar.
 
 import calendar
+#import emoji #https://unicode.org/emoji/charts/emoji-list.html
 
 # MM-DD, days of Fast outside of Wed/Fri
-daysOfFast = ['12-24','01-05','08-29','09-14']
+daysOfFast = ['12-24','1-5','8-29','9-14']
+theotokoFast = ['8-1','8-2','8-3','8-4','8-5','8-6','8-7','8-8'
+				, '8-9','8-10','8-11','8-12','8-13','8-14']
+
+def typeOfAllowedEmoji(level):
+	# positive messages instead
+	return ""
+
+def typeOfAbstinenceEmoji(level):
+	if level == 1: #Days of Fast, Christmas Fast (Wed, Fri), Theotoko's fast (except Sat/Sun)
+		#return "No meat, dairy, eggs, fish (except shellfish), olive oil, and wine."		
+		return ':cross_mark: :meat_on_bone:,:cheese_wedge:,:egg:,:fish:,:amphora:,:wine_glass:'
+	elif level == 2: #Theotoko's fast (Sat/Sun), some Feast Days 
+		return ':cross_mark: :meat_on_bone:,:cheese_wedge:,:egg:,:fish:'
+	elif level == 3: #Christmas Fast (Sun, Mon, Tues, Thurs, Sat), Holy Apostles
+		return ':cross_mark: :meat_on_bone:,:cheese_wedge:,:egg:'
+	elif level == 4:
+		return ':cross_mark: :meat_on_bone:'
+	else:
+		return ""
 
 def typeOfAbstinence(level):
-	if level == 1: #Days of Fast, Christmas Fast (Wed, Fri)
-		return "No meat, dairy, fish (except shellfish), olive oil, and wine."
-	elif level == 2: #Christmas Fast (Sun, Mon, Tues, Thurs, Sat)
-		return "No meat and dairy."
+	if level == 1: #Days of Fast, Christmas Fast (Wed, Fri), Theotoko's fast (except Sat/Sun)
+		return "No meat, dairy, eggs, fish (except shellfish), olive oil, and wine."
+	elif level == 2: #Theotoko's fast (Sat/Sun), some Feast Days 
+		return "No meat, dairy, eggs, fish (except shellfish)."
+	elif level == 3: #Christmas Fast (Sun, Mon, Tues, Thurs, Sat), Holy Apostles
+		return "No meat, dairy, and eggs."
+	elif level == 4:
+		return "No meat."
 	else:
-		return "No fast."
+		return ""
 
 def monthList(date):
 
@@ -43,11 +67,13 @@ def determineFast(weekdayInput, dayInput, monthInput, yearInput):
 	weekday = int(weekdayInput) # 0 is Mon, 1 is Tues...
 
 	combinedDate = str(month) + '-' + str(day)
-	dateType = typeOfAbstinence(0) #default
+	dateType = typeOfAbstinenceEmoji(0) #default
 
 	if combinedDate in daysOfFast:
-		dateType = typeOfAbstinence(1)
+		dateType = typeOfAbstinenceEmoji(1)
+	elif combinedDate in theotokoFast and not(weekday == 2 or weekday == 4):
+		dateType = typeOfAbstinenceEmoji(3)
 	elif weekday == 2 or weekday == 4: #Wed or Fri
-		dateType = typeOfAbstinence(1)
+		dateType = typeOfAbstinenceEmoji(1)
 
 	return dateType
